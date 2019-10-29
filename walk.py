f7c5c616268feb5bad4b5974fce27258098ecaa9
leonardo221097 <leoalca22@gmail.com>
2019-10-29 18:39:00 +0000

    ejercicio23

---
+++ walk.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+a = os.system("c++ walk.cpp -o walk.x")
+a = os.system("./walk.x > walk.dat")
+
+plt.figure()
+data = np.loadtxt("walk.dat")
+plt.plot(data[:,0], data[:,1])
+plt.axis('square')
+plt.xlim([-25, 50])
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.savefig("walk.png")
